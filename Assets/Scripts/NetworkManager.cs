05bdf0db26287a6328348c07d95e3e2a43510a25
kmk0105-S2 <11morgan@naver.com>
2024-07-31 18:19:43 +0900

    시작->로비

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -6,22 +6,50 @@ using Photon.Realtime;
 using UnityEngine.UI;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
-{
-    public Text StatusText;
-    public InputField roomInput, NickNameInput;
-
+{   
     private void Awake() => Screen.SetResolution(1280, 720, false);
-
-    private void Update() => StatusText.text = PhotonNetwork.NetworkClientState.ToString();
     // 네트워크 상태 표시
 
-    public void OnConnectedToServer() => PhotonNetwork.ConnectUsingSettings();
-    //서버 접속
+    public void OnClickStartButton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Start Button Clicked");
+        Info();
+    }
+    // 서버 접속
 
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+ 
         PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 5 }, null);
     }
 
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("LobbyScene");
+    }
+    // 로비 씬으로 전환
+
+    [ContextMenu("정보")]
+    void Info()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            print("현재 방 이름 : " + PhotonNetwork.CurrentRoom.Name);
+            print("현재 방 인원수 : " + PhotonNetwork.CurrentRoom.PlayerCount);
+            print("현재 방 최대인원수 : " + PhotonNetwork.CurrentRoom.MaxPlayers);
+
+            string playerStr = "방에 있는 플레이어 목록 : ";
+            for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++) playerStr += PhotonNetwork.PlayerList[i].NickName + ", ";
+            print(playerStr);
+        }
+        else
+        {
+            print("접속한 인원 수 : " + PhotonNetwork.CountOfPlayers);
+            print("방 개수 : " + PhotonNetwork.CountOfRooms);
+            print("모든 방에 있는 인원 수 : " + PhotonNetwork.CountOfPlayersInRooms);
+            print("로비에 있는지? : " + PhotonNetwork.InLobby);
+            print("연결됐는지? : " + PhotonNetwork.IsConnected);
+        }
+    }
 }
